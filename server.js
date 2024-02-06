483e3edf37559ee76f2320a660a78ec08b9047bf
slayerstokely <stokely@slayers.ru>
2024-02-06 22:53:29 +0200

    fixed __dirname

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import users_router from "./routes/users.routes.js";
 import media_router from "./routes/media.routes.js";
@@ -8,6 +9,9 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
